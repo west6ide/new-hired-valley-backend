d01540b91c62bcdf323388b6ebf24f884373eea0
Sultan <skydyrbaev66@gmail.com>
2024-11-14 09:35:19 +0500

    story func create

--- models/recommend/content.go
+++ models/recommend/content.go
@@ -1,11 +1,34 @@
 package recommend
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+)
+
 // Content представляет структуру одной рекомендации
 type Content struct {
-	Title       string   `json:"title"`       // Название контента
-	Description string   `json:"description"` // Описание или краткое содержание
-	ContentURL  string   `json:"content_url"` // Ссылка на материал
-	Category    string   `json:"category"`    // Категория (например, "Программирование", "Финансы")
-	SkillLevel  string   `json:"skill_level"` // Уровень сложности (например, "Новичок", "Средний", "Эксперт")
-	Tags        []string `json:"tags"`        // Теги, описывающие содержание
+	Title       string      `json:"title"`
+	Description string      `json:"description"`
+	ContentURL  string      `json:"content_url"`
+	Category    string      `json:"category"`
+	SkillLevel  string      `json:"skill_level"`
+	Tags        StringArray `json:"tags" gorm:"type:json"` // Используем кастомный тип для массива строк
+}
+
+// StringArray представляет собой кастомный тип для []string, чтобы сохранять его как JSON в базе данных
+type StringArray []string
+
+// Value сериализует массив в JSON при сохранении в базе данных
+func (a StringArray) Value() (driver.Value, error) {
+	return json.Marshal(a)
+}
+
+// Scan десериализует JSON из базы данных в массив
+func (a *StringArray) Scan(value interface{}) error {
+	b, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("unsupported data type: %T", value)
+	}
+	return json.Unmarshal(b, &a)
 }
